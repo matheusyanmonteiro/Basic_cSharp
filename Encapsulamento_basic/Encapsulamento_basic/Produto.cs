20f815d6c10b7bc20187b7e39eb324482339a9c5
Matheus Yan Monteiro <matheus.monteiro@confea.local>
2021-02-12 15:20:43 -0300

    updated projact base get, set and properties

---
+++ Encapsulamento_basic/Encapsulamento_basic/Produto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento_basic
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome)
+        {
+            _nome = nome;
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        // method get and set
+        //nome  os get e set foram substituidos pela função props uma ferramenta do c#.
+        /*public string GetNome()
+        {
+            return _nome;
+        }
+        
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }*/
+        //preco
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            _preco = preco;
+        }
+        //Quantidade
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade)
+        {
+            _quantidade = quantidade;
+        }
+
+        //props in c#
+
+        public string Nome
+        {
+            get { return _nome; }
+            set 
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Encapsulamento_basic/Encapsulamento_basic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Encapsulamento_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("diego");
+
+            Console.WriteLine(p.Nome);
+
+            p.Nome = "TV";
+
+            Console.WriteLine(p.Nome);
+
+        }
+    }
+}
